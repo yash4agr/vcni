4d026f268b0c5bfe7a77735a488cdb5a7f58d8cb
Yash Agrawal <yash4agr@gmail.com>
2026-01-22 16:51:09 +0530

    chore: add .gitignore and fix favicon loading

--- frontend/src/components/Head.tsx
+++ frontend/src/components/Head.tsx
@@ -3,6 +3,8 @@ export const Head = () => {
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/png" href="/icon.png" />
+      <title>VCNI - Voice Assistant</title>
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
